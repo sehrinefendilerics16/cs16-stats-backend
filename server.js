0da281b2567c0cd38e9c96bbf594f0497431df89
sehrinefendilerics16 <leventistemi@gmail.com>
2026-04-04 18:01:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const app = express();
 
 app.use(express.json());
 
+// Veri alma endpointi
 app.post("/data", (req, res) => {
-    console.log("GELEN VERİ:", req.body);
-    res.send("OK");
+  console.log("GELEN VERİ:", req.body);
+  res.send("OK");
 });
 
+// Test endpointi
 app.get("/", (req, res) => {
-    res.send("SERVER CALISIYOR");
+  res.send("SERVER CALISIYOR");
 });
 
-app.listen(3000, () => {
-    console.log("Server calisiyor");
+// PORT fix (KRİTİK)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server calisiyor, port:", PORT);
 });
